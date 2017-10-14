beb689f25457315aac52654ce68912b4cd9f9642
yoga <yogamurtisutanto@gmail.com>
2017-10-14 14:46:17 -0700

    flight search

--- googleAssistant/index.js
+++ googleAssistant/index.js
@@ -1,14 +1,49 @@
 import { ApiAiAssistant } from 'actions-on-google'
 import * as apiAiActions from '../apiActions'
+var request = require('request')
 
 export function apiAssistant(request, response) {
   const assistant = new ApiAiAssistant({ request: request, response: response })
   
   async function greetUser(assistant) {
-    return assistant.ask('hello singapore airlines!')
+    console.log(assistant)
+    return assistant.ask('hi singapore airlines!')
   }
   
   async function searchFlights(assistant) {
+    console.log(assistant.data)
+    if (assistant.data !== undefined) {
+      console.log(assistant.data["geo-country.original"])
+      console.log(assistant.data["geo-city1.original"])
+      // get departure date
+      // get return date
+      // get number of adults travelling
+      var params = {  
+         "clientUUID":"daredevil123",
+         "request":{  
+            "itineraryDetails":[  
+               {  
+                  "originAirportCode":"SIN",
+                  "destinationAirportCode":"FCO",
+                  "departureDate":"2017-11-01"
+               },
+               {  
+                  "originAirportCode":"FCO",
+                  "destinationAirportCode":"SIN",
+                  "departureDate":"2017-11-08"
+               }
+            ],
+            "cabinClass":"Y",
+            "adultCount":1
+         }
+      }
+      request.post({
+        uri: 'https://apidev.singaporeair.com/appchallenge/flight/search',
+        form: params
+      }, function(error, response, body) {
+        console.log(body);
+      })
+    }
     console.log('hiiii')
     return assistant.ask('found it!')
   }
