9bc1408e86a7863d5c5c8666fc46fdb6817c7a6c
abanik <abanik@paypal.com>
2017-11-11 10:30:45 -0800

    first commit

--- apiActions/index.js
+++ apiActions/index.js
@@ -1,24 +1,4 @@
 // API.AI actions
-export function searchFlights() {
-  return 'search_flights'
-}
-
-export function bookFlight() {
-  return 'book_flight'
-}
-
 export function welcomeIntent() {
   return 'input.welcome'
-}
-
-export function unhandled() {
-  return 'input.unknown'
-}
-
-export function visitLocationSelected() {
-  return 'actions.intent.OPTION'
-}
-
-export function checkIn() {
-  return 'check_in'
 }
